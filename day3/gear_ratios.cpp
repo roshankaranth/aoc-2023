0a5321e4aed9f7246d5ef95a850758cbb7bfb377
roshankaranth <roshankaranth@gmail.com>
2023-12-03 13:45:45 +0530

    gear ratios day 3

---
+++ day3/gear_ratios.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    ifstream in("input.txt");
+    string str;
+    vector<string> vstr;
+
+    for(int i = 0 ; getline(in,str); i++){
+        vstr.push_back(str);
+    }
+
+    long long sum=0;
+    for(int i = 0 ; i< vstr.size();i++){
+        int number = 0;
+        int flag = 0;
+        
+        for(int j = 0 ; j < vstr[i].size();j++){
+            int start,end;
+            if(vstr[i][j]>=48 && vstr[i][j]<=57){
+                number = number*10 + (vstr[i][j]-'0');
+                if(flag==0){
+                    start = j-1;
+                    flag=1;
+                }
+                end = j+1;
+               
+                
+            }
+            
+            if((vstr[i][j]<48 || vstr[i][j]>57 || vstr[i][j+1] == '\0')  && number!=0){
+                
+                if(start==-1){
+                    start = 0;
+                }
+
+                if(end==vstr[i].size()){
+                    end = vstr[i].size()-1;
+                }
+                while(start<=end){
+                    for(int k = -1 ; k <=1 ; k++){
+                        if((i==0 && k==-1) || (i==vstr.size()-1 && k==1)){
+                            continue;
+                        }
+                        if((vstr[i+k][start]<48 || vstr[i+k][start]>57) && vstr[i+k][start]!='.'){
+                            sum += number;
+                            start = 1, end=0;
+                            break;
+                        }
+                    }
+                    start++;
+                }
+                start=0;
+                number = 0;
+                end = 0;
+                flag =0;
+                
+            }
+        }
+    }
+
+    cout<<sum;
+    return 0;
+}
