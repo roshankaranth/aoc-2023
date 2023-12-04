572aaf9c61cbfc68051dd1a495aedffeb2858716
roshankaranth <roshankaranth@gmail.com>
2023-12-04 09:50:24 +0530

    day2-2

---
+++ day2/p2.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    string str;
+    ifstream in("input.txt");
+    int sum;
+
+    for(int k = 0 ; k < 100 ; k++){
+    getline(in,str);
+    
+    int game_id=0;
+    for(int i = 5; str[i]!=':';i++){
+        game_id = game_id*10 + (str[i]-'0');
+    }
+    int red = 0;
+    int blue = 0 ;
+    int green = 0;
+    
+    for(int i = 7 ; str[i]!= '\0';){
+        int number = 0;
+        while(str[i]!=';' && str[i+1]!='\0'){
+            
+            if(str[i]>=48 && str[i]<=57){
+                number = number*10 + (str[i] - '0');
+
+            
+        }else if(str[i]==' '){
+            if(str[i+1]=='r'){
+                if(red<number){
+                    red = number;
+                }
+                number = 0;
+            }else if(str[i+1]=='g'){
+                if(green<number){
+                    green = number;
+                }
+                number = 0;
+            }else if(str[i+1]=='b'){
+                if(blue<number){
+                    blue = number;
+                }
+                number = 0;
+            }
+        }
+        i++;
+        }
+        
+        i++;
+    }
+    sum+= red*blue*green;
+
+}
+    cout<<sum;
+    return 0;
+}
+
+//12 red cubes, 13 green cubes, and 14 blue cubes
